47aef23ef7d3a5eec0a46d08f5751264e5aaa75d
Tianli Ding <td2@illinois.edu>
2019-12-17 17:42:56 -0600

    DTL commit

--- app.R
+++ app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(readr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -34,13 +35,12 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    dataset = read.csv("profiles-2.csv")
+    MyCupid = read.csv("MyCupid.csv")
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         
-        x    <- faithful[,1]
+        x    <- MyCupid[,2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
